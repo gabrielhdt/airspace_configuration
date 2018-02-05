4412fde15cac91cb4bc03065d3f4589f144bc4a6
gabrielhdt <gabrielhondet@gmail.com>
2018-02-05 14:40:34 +0100

    (+) continued interface

--- src/airconf.mli
+++ src/airconf.mli
@@ -3,6 +3,12 @@
 (** Specification of a configuration *)
 type t
 
+(** A default state *)
+val dummy : t
+
 (** [confcost t] gives the forecasted cost of config [t], allows one to
     compare confs *)
 val confcost : t -> float
+
+(** [produce t] returns all feasible configurations from configuration [t] *)
+val produce : t -> t list
