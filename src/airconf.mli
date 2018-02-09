e528f3aee95c5fc190a61e40eb59fb39752752cb
Benoit Viry <benoit.viry@laposte.net>
2018-02-09 14:49:57 +0100

    (+) test cost path + doc

--- src/airconf.mli
+++ src/airconf.mli
@@ -3,18 +3,21 @@
 (** Specification of a configuration *)
 type t
 
-(** A default state *)
-val dummy : t
-
-(** [confcost t] gives the actual cost of config [t], allows one to
+(** [confcost t ( int -> string -> int ) ] gives the actual cost of config [t],
+    according to all the flights in the sector. allows one to
     compare configurations *)
-val confcost : t  -> float
+val conf_cost : t -> ( int -> string -> int ) -> float
 
 (** [confcost t -> t ] gives the cost of transition [t -> t],
     allows one to compare transitions *)
-val tracost : t -> t -> float
+val trans_cost : t -> t -> float
 
-val path_cost : t -> ( t -> float ) -> ( t -> t -> float ) -> float
+(** [path_cost ( int -> string -> int ) t list ( t -> ( int -> string -> int ) -> float )
+    ( t -> t -> float )] gives the cost of a given path [t list] for all the
+    flights in the sector at each timestep*)
+val path_cost : ( int -> string -> int ) -> t list ->
+  ( t -> ( int -> string -> int ) -> float ) ->
+  ( t -> t -> float ) -> float
 
 (** [produce t] returns all feasible configurations from configuration [t] *)
 val produce : t -> t list
@@ -22,3 +25,8 @@ val produce : t -> t list
 (** [terminal t] asserts whether a configuration [t] is the last to be
     considered *)
 val terminal : t -> bool
+
+(** [make_conf int (Util.Sset.t * Util.Smap.key list) list] create a new
+    configuration for a given timestep [t] and partition
+    [(Util.Sset.t * Util.Smap.key list) list]*)
+val make_conf : int -> (Util.Sset.t * Util.Smap.key list) list -> t
--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,33 @@
+let f t s =
+  let l = [("1", [5; 4; 3]); ("2", [2; 2; 4])
+          ; ("3", [1; 1; 2]); ("4", [3; 4; 5]);
+           ("5", [4; 5; 5]) ] in
+  List.nth (List.assoc s l) t
+
 let () =
-  let scpath = Sys.argv.(1) in
+  (* let scpath = Sys.argv.(1) in
   let sc = Scenario.load scpath in
-  let f = Scenario.f sc in
+
+  let f = Scenario.f sc in *)
+
+  let s01 = Util.Sset.add "3" (Util.Sset.add "2"
+      (Util.Sset.add "1" Util.Sset.empty)) in
+  let s02 = Util.Sset.add "5"
+      (Util.Sset.add "4" Util.Sset.empty) in
+  let conf0 = Airconf.make_conf 0 [(s01, ["a"]); (s02, ["b"])] in
+
+  let s11 = Util.Sset.add "1" (Util.Sset.add "2" Util.Sset.empty) in
+  let s12 = Util.Sset.add "3" (Util.Sset.add "4"
+      (Util.Sset.add "5" Util.Sset.empty)) in
+  let conf1 = Airconf.make_conf 1 [(s11, ["c"]); (s12, ["d"])] in
+
+  let s21 = Util.Sset.add "1" (Util.Sset.add "2" Util.Sset.empty) in
+  let s22 = Util.Sset.add "3" (Util.Sset.add "4"
+      (Util.Sset.add "5" Util.Sset.empty)) in
+  let conf2 = Airconf.make_conf 2 [(s21, ["c"]); (s22, ["d"])] in
+
+  let path = [conf0; conf1; conf2] in
+
+  let c = Airconf.path_cost f path Airconf.conf_cost Airconf.trans_cost in
+  Printf.printf "%f\n" c;
   ()
