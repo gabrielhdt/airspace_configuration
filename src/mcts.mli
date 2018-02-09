551c4036857b30991ca993b1d19e55a6b0d332aa
Gabriel Hondet <gabrielhondet@gmail.com>
2018-02-09 14:07:09 +0100

    (~) reworked header

--- src/mcts.mli
+++ src/mcts.mli
@@ -1,5 +1,12 @@
 (** Tree type to explore. Genericity is relevant for functor only *)
 type 'a tree
 
-(** [mcts r] updates tree of root [t] with monte carlo *)
-val mcts : Airconf.t tree -> unit
+(** [best_path_max t n] @returns a path of nodes from the root of [t] to a
+    terminal leaf. The selection is based on the reward in each node. *)
+val best_path_max : Airconf.t tree -> int -> Airconf.t tree list
+
+(** Selects nodes which have the highest visit count *)
+val best_path_secure : Airconf.t tree -> int -> Airconf.t tree list
+
+(** Selects nodes which minimise a lower confidence bound *)
+val best_path_robust : Airconf.t tree -> int -> Airconf.t tree list
