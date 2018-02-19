434080c8002907a8ad77b9a534d861dd3f0d26da
gabrielhdt <gabrielhondet@gmail.com>
2018-02-19 23:03:40 +0100

    (+) added cl params

--- src/main.ml
+++ src/main.ml
@@ -1,11 +1,17 @@
 let usage = "Usage: " ^ Sys.argv.(0) ^ " " ^
-            "[-scenario string]"
+            "-scenario string [-nsim int] [-tmax int]"
 
 let scpath = ref ""
+let nsim = ref 8
+let tmax = ref 5
 
 let speclist = [
   ("-scenario", Arg.Set_string scpath,
-   "scenario to load and play") ;
+   "Scenario to load and play") ;
+  ("-nsim", Arg.Set_int nsim,
+   "Number of simulations carried out for each selection") ;
+  ("-tmax", Arg.Set_int tmax,
+   "Depth of the tree") ;
 ]
 let () =
   Arg.parse speclist (fun x -> raise (Arg.Bad ("bad argument: " ^ x))) usage ;
@@ -22,7 +28,7 @@ let () =
   let module Airmcts = Mcts.Make(Support) in
   let root = Airmcts.make_node (Support.make_root initial_partition) in
 
-  let b_path = (Airmcts.best_path_robust root 7) in
+  let b_path = (Airmcts.best_path_robust root !nsim) in
   Printf.printf "%d\n" (List.length b_path);
 
   Partitions.print_partitions
