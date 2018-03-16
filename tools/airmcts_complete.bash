15198d0cc84db7518beac3f87d8816f0146d623b
Gabriel Hondet <gabrielhondet@gmail.com>
2018-03-16 13:53:40 +0100

    (+) bash completion works

--- tools/airmcts_complete.bash
+++ tools/airmcts_complete.bash
@@ -3,17 +3,18 @@ _comp_airmcts ()
 {
 	local cur prev options
 	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	prev="${COMP_WORDS[COMP_CWORD - 1]}"
 
 	options="-scenario -maxtime -verbose -alpha -beta -gamma -delta -theta"
 	case "$prev" in
 		"-scenario")
-			COMPREPLY=( $(compgen -d) )
+			COMPREPLY=( $(compgen -f -- ${cur}) )
+			return 0
+			;;
+		*)
+			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
 			return 0
 			;;
 	esac
-	COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
-	return 0
-
 }
-complete -o bashdefault -F _comp_airmcts airmcts
+complete -o bashdefault -o filenames -F _comp_airmcts airmcts
