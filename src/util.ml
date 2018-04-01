03a6cbb3965586fd1cd0cf9e0b8f98e67cb63fd0
gabrielhdt <gabrielhondet@gmail.com>
2018-04-01 11:18:41 +0200

    (~) formatted (ocamlformat) files
    And removed tests

--- src/util.ml
+++ src/util.ml
@@ -1,6 +1,13 @@
+module Sset = Set.Make (struct
+  type t = string
 
-module Sset= Set.Make (struct type t=string let compare=compare end);;
-module Smap= Map.Make (struct type t=string let compare=compare end);;
+  let compare = compare
+end)
 
-let set_of_list l=
-  List.fold_left (fun acc x -> Sset.add x acc) Sset.empty l;;
+module Smap = Map.Make (struct
+  type t = string
+
+  let compare = compare
+end)
+
+let set_of_list l = List.fold_left (fun acc x -> Sset.add x acc) Sset.empty l
