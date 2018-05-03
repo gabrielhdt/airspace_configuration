5f6949c9de2a84513d9ef71aeff27fc3bd711aa0
gabrielhdt <gabrielhondet@gmail.com>
2018-05-03 17:54:54 +0200

    (+) More control, more args

--- script/mcts_perf.bash
+++ script/mcts_perf.bash
@@ -1,18 +1,93 @@
 #!/bin/bash
 
+NAME=$0
+OPTIONS=hu:l:n:o:s:d:
+LONGOPTIONS=help,scenario:,depth:,upper:,lower:,nval:,out:
+USAGE="Usage: $0 -s <scenario> -d <depth>
+Param:
+\t-l|--lower\t<float>\tlower bound of time per step
+\t-u|--upper\t<float>\tupper bound of time per step
+\t-n|--nval\t<int>\tnumber of measures
+\t-o|--out\t<filepath>\toutput file"
+
+SCENARIO=""
+DETPH=0
+OUTFILE='tps.data'
+lowerbound=0.0001
+upperbound=0.1
+nval=10
+
 MCTS_CMD="airmcts"
 
+temp=$(getopt -o $OPTIONS --long $LONGOPTIONS -n $NAME -- "$@")
+
+if [[ $? -ne 0 ]]; then
+    echo "Terminating..." >&2
+    exit 1
+fi
+
+eval set -- "$temp"
+unset temp
+while true; do
+    case "$1" in
+        '-s'|'--scenario')
+            SCENARIO=$2
+            shift 2
+            continue
+            ;;
+        '-d'|'--depth')
+            DEPTH=$2
+            shift 2
+            continue
+            ;;
+        '-o'|'--out')
+            OUTFILE=$2
+            shift 2
+            continue
+            ;;
+        '-u'|'--upper')
+            upperbound=$2
+            shift 2
+            continue
+            ;;
+        '-l'|'--lower')
+            lowerbound=$2
+            shift 2
+            continue
+            ;;
+        '-n'|'--nval')
+            nval=$2
+            shift 2
+            continue
+            ;;
+        '--')
+            shift
+            break
+            ;;
+        '-h'|'--help')
+            echo -e "$USAGE"
+            exit 1
+            ;;
+        *)
+            echo -e "Wrong args"
+            echo -e "$USAGE"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -f $OUTFILE ]] ; then
+    rm -i $OUTFILE
+fi
+
 # Time per step test
-lowerbound=0.0000001
-upperbound=0.01
-nval=200
 step=$( echo "($upperbound - $lowerbound) / $nval" | bc -l )
 echo "step of $step"
-rm tps.out
 for i in $(seq 1 $nval) ; do
-	printf "$i/$nval\r"
-	val=$( echo "$lowerbound + $i * $step" | bc -l )
-	out="$($MCTS_CMD -nsteps 5 -timeperstep $val -scenario $1)"
-	echo "$val $out" >> tps.out
+    printf "$i/$nval\r"
+    val=$( echo "$lowerbound + $i * $step" | bc -l )
+    out="$( $MCTS_CMD -nsteps $maxsteps -horizon $maxsteps -timeperstep $val \
+        -scenario $SCENARIO )"
+    echo "$val $out" >> $OUTFILE
 done;
-gnuplot -p -e 'plot "tps.out"'
+gnuplot -p -e "plot \"$OUTFILE\" with lines"
